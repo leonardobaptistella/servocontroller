9a67db57e74b9599f191d788fb963c84b9ed484c
Leonardo B <leonardo.ribeirobaptistella@usp.br>
2022-05-11 17:23:39 -0300

    teste

--- angulo.py
+++ angulo.py
@@ -1,4 +1,9 @@
 # Função de posicionamento da câmera p/ ângulo fixo
 
-def angulo(theta):
-    return 0
+def angulo(pos, p_servo):
+
+    servo = Servo(p_servo)
+
+    if(pos == '1'): servo.max()
+    if(pos == '0'): servo.mid()
+    if(pos == '-1'): servo.min()
