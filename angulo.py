17b26c813eced3fc02eb01ef0fad7313413812b1
leonardo jose ribeiro baptistella <39542216+leonardobaptistella@users.noreply.github.com>
2022-06-01 16:43:09 -0300

    Delete angulo.py

--- angulo.py
+++
@@ -1,9 +0,0 @@
-# Função de posicionamento da câmera p/ ângulo fixo
-
-def angulo(pos, p_servo):
-
-    servo = Servo(p_servo)
-
-    if(pos == '1'): servo.max()
-    if(pos == '0'): servo.mid()
-    if(pos == '-1'): servo.min()
